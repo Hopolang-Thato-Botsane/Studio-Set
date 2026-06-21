f04c0fc2d1ac358ad9ade49356538a1eb1073e2f
Hopolang-Thato-Botsane <HopolangBotsane@outlook.com>
2026-06-21 18:45:48 +0200

    feat(footer): add unified footer component

--- studio-and-set/src/app/(marketing)/page.tsx
+++ studio-and-set/src/app/(marketing)/page.tsx
@@ -1,9 +1,11 @@
 import Storefront from "@/components/Storefront/Storefront";
+import Footer from "@/components/Footer/Footer"
 
 export default function Home() {
   return (
     <main>
       <Storefront />
+      <Footer/>
     </main>
   );
 }
