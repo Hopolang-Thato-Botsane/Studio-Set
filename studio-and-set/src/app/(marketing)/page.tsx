82786e2ff2c3a80c26121199fd2fd6049cfe7720
Hopolang-Thato-Botsane <HopolangBotsane@outlook.com>
2026-06-22 09:46:42 +0200

    feat(process): - add process section cards - made them responsive - display them on the home page

--- studio-and-set/src/app/(marketing)/page.tsx
+++ studio-and-set/src/app/(marketing)/page.tsx
@@ -1,9 +1,11 @@
+import Process from "@/components/Process/Process"
 import Storefront from "@/components/Storefront/Storefront";
 import Footer from "@/components/Footer/Footer"
 
 export default function Home() {
   return (
     <main>
+      <Process/>
       <Storefront />
       <Footer/>
     </main>
